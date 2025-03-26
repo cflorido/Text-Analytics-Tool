fe863793608fcf5c27c79905545d2b838ee8aed5
Marti820 <j.vasquezc@uniandes.edu.co>
2025-03-26 12:03:32 -0500

    navegacion

--- Fase 2/Front/app.py
+++ Fase 2/Front/app.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/predecir')
+def predecir():
+    return render_template('predicir.html')
+
+@app.route('/prediccionesArchivo')
+def predecirArchivo():
+    return render_template('predecirArchivo.html')
+
+@app.route('/reentreno')
+def reentreno():
+    return render_template('reentreno.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
