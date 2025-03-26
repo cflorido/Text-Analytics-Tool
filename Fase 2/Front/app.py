acf39e44c438a2bbf5b471519c0d148adf2d3047
Marti820 <j.vasquezc@uniandes.edu.co>
2025-03-26 15:00:01 -0500

    clasificar una hecho (fake API)

--- Fase 2/Front/app.py
+++ Fase 2/Front/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, jsonify, render_template, request, send_from_directory
 
 app = Flask(__name__)
-
+#------------Front------------------
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -21,6 +21,45 @@ def reentreno():
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory('static', 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+#----------Interaccion con el API-----------
 
+@app.route('/clasificarEnvio', methods=["POST"])
+def clasificarEnvio():
+    titulo = request.form.get("titulo") 
+    cuerpo = request.form.get("cuerpo") 
+
+    #--------------------Envio de texto a la API (HACER)------------------
+    a ="""     payload = {
+        "titulo": titulo,
+        "cuerpo": cuerpo
+    }
+    try:
+       response = requests.post('http://API/clasificar', json=payload)
+        if response.status_code == 200:
+                return jsonify(response.json()) 
+                ...
+        else:
+            return jsonify({"error": "Failed to get a valid response from the API"}), response.status_code
+            ...
+        except requests.exceptions.RequestException as e:
+            return jsonify({"error": f"Request failed: {str(e)}"}), 500 """
+    # --------------------RESPUESTA FALSA DEL API-----------------
+    
+    #La respuesta es un json que hay que hacerle parsing
+    
+    fake_response = {
+        "titulo": titulo,
+        "cuerpo": cuerpo,
+        "clasificacion": "Verdadero" if "ejemplo" in titulo.lower() else "Falso",
+        "ProbVerdaderpa": 0.85,  
+        "ProbFalsa": 0.15
+    }
+    response_dict =  fake_response
+    
+    #return response_json    
+    return render_template('clasificar.html', results=response_dict)
+
+
+#----------Main--------------------------
 if __name__ == '__main__':
     app.run(debug=True)
